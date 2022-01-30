5247608e68ddd486d6348eda43690cbee9489ce2
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-01-30 13:52:06 +0100

    Add function fetch with weather local API and show in page

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=50.8624896&lon=20.5914112&appid=74a450b82ce39d2bf37dca659bc634d7').then(data => data.json()).then(data => {
+    console.log(data)
+    dataWeather(data);
+});
+
+function dataWeather(data) {
+    const nameCity = document.getElementById('nameCity');
+    nameCity.innerHTML = data.name;
+
+    const localTime = document.getElementById('localTime');
+    localTime.innerHTML = data.timezone;
+
+    const valueTemperature = document.getElementById('valueTemperature');
+    valueTemperature.innerHTML = data.main.temp;
+
+    const windySpeed = document.getElementById('windySpeed');
+    windySpeed.innerHTML = data.wind.speed;
+}
